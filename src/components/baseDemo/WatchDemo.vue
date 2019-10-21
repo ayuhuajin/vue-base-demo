05bc5bc4fffed6b43da8f6fab76a6984d34f81e9
wqh <wqh@nxin.com>
2019-10-21 15:55:43 +0800

    watch Demo

--- src/components/baseDemo/WatchDemo.vue
+++ src/components/baseDemo/WatchDemo.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="watch-demo">
-    454545
+    <!-- .sync 的使用 -->
+    <input type="number" :value="num" @input="changeNum" />
+    <!-- 监听title -->
+    <input type="text" :value="title" @input="changeTitle($event, 'title')" />
+    <input type="text" :value="titleImmediately" @input="changeTitle($event, 'immediateTitle')" />
+    <!-- 人员，监听对象 -->
+    <ul class="person">
+      <li v-for="(item, index) in persons" :key="index">
+        <span>姓名:{{ item.name }}</span>
+        <span>年龄:{{ item.age }}</span>
+        <span @click="handleDel(index)">删除</span>
+        <span @click="handleAdd(index)">添加</span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,7 +23,92 @@ import Vue from 'vue';
 export default Vue.extend({
   name: 'WatchDemo',
   props: {
-    msg: String
+    num: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      default: '标题'
+    },
+    titleImmediately: {
+      type: String
+    },
+    persons: {
+      type: Array, // 对象或数组默认值必须从一个工厂函数获取
+      default: function() {
+        return { name: '测试' };
+      }
+    }
+  },
+  watch: {
+    // 监听title
+    num(value) {
+      console.log('子级num', value);
+    },
+    // 监听title
+    title(value) {
+      console.log('子级title', value);
+    },
+    // 立即监听
+    titleImmediately: {
+      handler(newValue, oldVal) {
+        console.log('子级立即监听', newValue, oldVal);
+      },
+      immediate: true
+    },
+    // 人员 监听对象属性值得改变
+    persons: {
+      handler(newValue, oldVal) {
+        console.log('子级人员', newValue, oldVal);
+      },
+      deep: true
+    }
+  },
+  mounted() {
+    console.log((this as any).persons);
+  },
+  methods: {
+    changeNum(e: any) {
+      this.$emit('update:num', e.target.value);
+    },
+    // 改变标题
+    changeTitle(e: any, str: string) {
+      if (str === 'title') {
+        this.$emit('changeTitle', e.target.value);
+      } else if (str === 'immediateTitle') {
+        this.$emit('changeImmediatelyTitle', e.target.value);
+      }
+    },
+    // 删除人员
+    handleDel() {},
+    // 添加人员
+    handleAdd(index: number) {
+      (this as any).persons[index].name = Math.random();
+    }
   }
 });
 </script>
+
+<style lang="scss" scoped>
+.watch-demo {
+  margin-top: 20px;
+  padding: 0 20px;
+}
+input {
+  width: 150px;
+  margin-right: 20px;
+  padding-left: 6px;
+  border: none;
+  border: 1px solid#999;
+  border-radius: 5px;
+  line-height: 40px;
+  outline: none;
+}
+.person {
+  margin-top: 20px;
+  span {
+    margin-right: 15px;
+  }
+}
+</style>
--- src/views/BaseDemo.vue
+++ src/views/BaseDemo.vue
@@ -1,16 +1,75 @@
 <template>
   <div class="base-demo">
-    <watch-demo></watch-demo>
+    <watch-demo
+      :num.sync="num"
+      :title="title"
+      :titleImmediately="titleImmediately"
+      :persons="persons"
+      @changeTitle="changeTitle"
+      @changeImmediatelyTitle="changeImmediatelyTitle"
+    >
+    </watch-demo>
+    <span @click="changeValue">改变数值</span>
   </div>
 </template>
 
-<script>
-import WatchDemo from '@/components/baseDemo/WatchDemo';
-
+<script lang="ts">
+import WatchDemo from '@/components/baseDemo/WatchDemo.vue';
 export default {
   name: 'home',
   components: {
     WatchDemo
+  },
+  data() {
+    return {
+      num: 10,
+      title: '标题',
+      titleImmediately: '立即监听',
+      persons: [
+        // { id: 1, name: '张三', age: 25 },
+        // { id: 2, name: '李四', age: 26 },
+        // { id: 3, name: '王五', age: 100 },
+        // { id: 4, name: '陈留', age: 52 }
+      ] as any
+    };
+  },
+  watch: {
+    title() {
+      console.log('父级监听', this.title);
+    },
+    titleImmediately: {
+      handler(newValue: any, oldVal: any) {
+        console.log(newValue, oldVal);
+      },
+      immediate: true,
+      deep: true
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      (this as any).persons = [
+        { id: 1, name: '张三', age: 25 },
+        { id: 2, name: '李四', age: 26 },
+        { id: 3, name: '王五', age: 100 },
+        { id: 4, name: '陈留', age: 52 }
+      ];
+    }, 200);
+  },
+  methods: {
+    // 修改标题
+    changeTitle(title: string) {
+      (this as any).title = title;
+    },
+    // 修改立即监听的标题
+    changeImmediatelyTitle(title: string) {
+      (this as any).titleImmediately = title;
+    },
+    changeValue() {
+      (this as any).num = 9999;
+      (this as any).title = '123';
+      (this as any).titleImmediately = '456';
+      (this as any).persons = [{ id: 8, name: '李白', age: 100 }, { id: 9, name: '王五', age: 52 }];
+    }
   }
 };
 </script>
